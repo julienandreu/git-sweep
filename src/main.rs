62e22ec4430713d18cf4158a0d67dc6cd0519f81
Julien Andreu <julienandreu@me.com>
2024-07-06 14:00:31 -0400

    feat: Setup repo and add main git-sweep logic

---
+++ src/main.rs
@@ -0,0 +1,78 @@
+use std::{
+    env,
+    process::{exit, Command},
+    time::Instant,
+};
+
+fn git_fetch() {
+    Command::new("git")
+        .current_dir(env::current_dir().unwrap())
+        .args(["fetch"])
+        .output()
+        .expect("Failed to execute 'git fetch' command");
+}
+
+fn git_branch() -> Vec<String> {
+    let output = Command::new("git")
+        .current_dir(env::current_dir().unwrap())
+        .args(["branch"])
+        .output()
+        .expect("Failed to execute 'git branch' command");
+
+    if output.status.success() {
+        return String::from_utf8_lossy(&output.stdout.to_owned())
+            .lines()
+            .into_iter()
+            .filter_map(|branch| {
+                if branch.starts_with('*') {
+                    return None;
+                }
+
+                Some(branch.trim().to_owned())
+            })
+            .collect();
+    }
+
+    vec![]
+}
+
+fn git_branch_delete(branch: &str) -> String {
+    Command::new("git")
+        .current_dir(env::current_dir().unwrap())
+        .args(["branch", "-D", &branch])
+        .output()
+        .expect("Failed to execute 'git branch -D' command");
+
+    branch.to_owned()
+}
+
+fn format_branch_list(branches: &Vec<String>) -> String {
+    branches.join("\n ")
+}
+
+fn main() {
+    let start = Instant::now();
+
+    git_fetch();
+    let branches = git_branch();
+
+    if branches.is_empty() {
+        println!("No branch to sweep");
+        exit(0);
+    }
+
+    let sweeped_branches = branches
+        .into_iter()
+        .map(|branch| git_branch_delete(&branch))
+        .collect::<Vec<String>>();
+
+    let sweeped_message = format!(
+        "{} branch{} successfully sweeped in {:?}:\n {}",
+        sweeped_branches.len(),
+        if sweeped_branches.len() > 1 { "es" } else { "" },
+        start.elapsed(),
+        format_branch_list(&sweeped_branches)
+    );
+
+    println!("{}", sweeped_message);
+}
